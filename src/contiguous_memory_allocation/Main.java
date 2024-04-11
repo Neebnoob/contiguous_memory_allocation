65f22e92d7e861ad615c7600d877da8420ae2c89
Neebnoob <Neebnoob99@gmail.com>
2024-04-11 13:23:00 -0500

    Change to Main

--- src/contiguous_memory_allocation/Main.java
+++ src/contiguous_memory_allocation/Main.java
@@ -3,8 +3,9 @@ package contiguous_memory_allocation;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ConsoleGUI.startUp();
+		
 	}
 
 }
